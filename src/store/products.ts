b67047b97be02c35c9e3c761218ede950d513251
nageuna922@gmail.com <nageuna922@gmail.com>
2024-04-24 12:34:28 +0900

    Error: SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON 에러 발생으로 코드 수정

--- src/store/products.ts
+++ src/store/products.ts
@@ -4,7 +4,7 @@ import CONSTANTS from "../constants/constants";
 // 혹시 API통신이 되지 않는다면 /product.json파일을 활용해서 로드하세요.
 // const productsURL = "/products.json";
 const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
-// console.log(productsURL);
+console.log(productsURL);
 
 interface IRating {
   readonly rate?: number;
@@ -28,12 +28,7 @@ export const productsList = selector<IProduct[]>({
   key: "productsList",
   get: async () => {
     try {
-      const response = await fetch(productsURL, {
-        method: "GET",
-        headers: {
-          Accept: "application / json",
-        },
-      });
+      const response = await fetch(productsURL);
       return (await response.json()) || [];
     } catch (error) {
       console.log(`Error: \n${error}`);

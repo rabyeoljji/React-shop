37a5e64e26c593a7ee11f5f86da155c01f7b50c3
nageuna922@gmail.com <nageuna922@gmail.com>
2024-04-24 12:23:27 +0900

    - SearchBar 모바일 화면에서 아이콘 클릭 시 검색창 보이도록 코드 수정 - 콘솔코드 주석처리

--- src/components/layout/NavItems/SearchBar.tsx
+++ src/components/layout/NavItems/SearchBar.tsx
@@ -9,15 +9,14 @@ const SearchBar = (): JSX.Element => {
   const [theme, setTheme] = useRecoilState(themeState);
   const [searchValue, setSearchValue] = useState("");
   const [filterItems, setFilterItems] = useState<IProduct[]>([]);
+  const [clickActive, setClickActive] = useState<boolean>(false);
 
   useEffect(() => {
     let mounted = true;
-
     if (mounted && ProductsLoadable.state === "hasValue") {
       const filteredItems = filterProducts(ProductsLoadable.contents, searchValue);
       setFilterItems(filteredItems);
     }
-
     return (): void => {
       mounted = false;
     };
@@ -34,6 +33,9 @@ const SearchBar = (): JSX.Element => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
+  const handleClick = () => {
+    clickActive ? setClickActive(false) : setClickActive(true);
+  };
 
   return (
     <>
@@ -41,6 +43,7 @@ const SearchBar = (): JSX.Element => {
         <button
           type="button"
           className="flex sm:hidden w-10 sm:w-auto mx-0 px-0 sm:mx-2 sm:px-2 btn btn-ghost js-search"
+          onClick={() => handleClick()}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -57,15 +60,18 @@ const SearchBar = (): JSX.Element => {
           </svg>
         </button>
         <input
+          id="searchInput"
           type="text"
           placeholder="검색"
-          className={`fixed left-0 top-4 -z-10 opacity-0 sm:opacity-100 sm:static sm:flex w-full input input-ghost focus:outline-0 rounded-none sm:rounded ${
+          className={`fixed left-0 top-4 opacity-0 sm:opacity-100 sm:static sm:flex w-full input input-ghost focus:outline-0 rounded-none sm:rounded ${
             theme === "light" ? "bg-gray-300 text-gray-800" : "bg-gray-600 text-white"
-          } sm:transform-none transition-all js-searchInput`}
+          } sm:transform-none transition-all js-searchInput ${!clickActive ? "-z-10" : null} ${
+            clickActive ? "translate-y-full !opacity-100" : null
+          }`}
           value={searchValue}
           onChange={handleChange}
         />
-        {searchValue.length > 0 && (
+        {searchValue.length > 0 && clickActive && (
           <ul
             className={`!fixed left-0 sm:!absolute sm:top-14 menu flex-nowrap dropdown-content w-full sm:w-64 max-h-96 shadow text-base-content overflow-y-auto overflow-x-hidden ${
               theme === "light" ? "bg-white" : "bg-gray-600"
@@ -92,3 +98,5 @@ const SearchBar = (): JSX.Element => {
 };
 
 export default SearchBar;
+// 돋보기 아이콘 클릭 시 input 검색창 클래스에 translate-y-full !opacity-100 추가
+// fixed left-0 top-4 opacity-0 sm:opacity-100 sm:static sm:flex w-full input input-ghost focus:outline-0 rounded-none sm:rounded bg-gray-300 dark:bg-gray-600 !text-gray-800 dark:!text-white sm:transform-none transition-all js-searchInput translate-y-full !opacity-100

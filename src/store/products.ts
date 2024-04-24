8cd644b835aa725102d2458ed1ca9e4b00c6d027
nageuna922@gmail.com <nageuna922@gmail.com>
2024-04-24 12:51:53 +0900

    .

--- src/store/products.ts
+++ src/store/products.ts
@@ -3,7 +3,7 @@ import CONSTANTS from "../constants/constants";
 
 // 혹시 API통신이 되지 않는다면 /product.json파일을 활용해서 로드하세요.
 // const productsURL = "/products.json";
-const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `/${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
+const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
 console.log(productsURL);
 
 interface IRating {
@@ -28,7 +28,11 @@ export const productsList = selector<IProduct[]>({
   key: "productsList",
   get: async () => {
     try {
-      const response = await fetch(productsURL);
+      const response = await fetch(productsURL, {
+        headers: {
+          accept: "application/json",
+        },
+      });
       return (await response.json()) || [];
     } catch (error) {
       console.log(`Error: \n${error}`);

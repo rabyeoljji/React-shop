30bbe593ece4d09147ef224b5d57cae1f05b35c6
nageuna922@gmail.com <nageuna922@gmail.com>
2024-04-25 10:41:03 +0900

    - 배포 vercel 설정에서 .env등록 후 코드 변경

--- src/store/products.ts
+++ src/store/products.ts
@@ -2,9 +2,9 @@ import { selector } from "recoil";
 import CONSTANTS from "../constants/constants";
 
 // 혹시 API통신이 되지 않는다면 /product.json파일을 활용해서 로드하세요.
-const productsURL = "/products.json";
+// const productsURL = "/products.json";
 // const productsURL = "https://fakestoreapi.com/products";
-// const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
+const productsURL = `${CONSTANTS.IS_DEV ? `/proxy` : `${import.meta.env.VITE_FAKE_STORE_API}`}/products`;
 console.log(productsURL);
 
 interface IRating {
